2657d3f2b3540ae070d98a1eeb1c53b883e3048b
SilentCabin <75711387+SilentCabin@users.noreply.github.com>
2024-09-15 02:46:27 +0100

    <feat> complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+const repeatString = function(string, num) {
+    let word = ""; // create an empty variable to store our string
+    if (num < 0) {
+        word = "ERROR" // error if less than 0
+    } else{
+        
+        for (let i = 0; i < num; i++) { // initiate counter for the amount of defined times
+            word += string;    // change our empty variable to our string, however many times it needs to be repeated
+            
+        }}
+        return word; // return final string
+    };
+    
+    // Do not edit below this line
+    module.exports = repeatString;
+    
