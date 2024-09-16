c4885a91285119ebbd8407920c8f724ff1036fa8
SilentCabin <75711387+SilentCabin@users.noreply.github.com>
2024-09-16 03:58:32 +0100

    <feat> write reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringLength = string.length;
+    let reversedString = "";
+    for (let i = stringLength -1; i >= 0; i--) {
+        reversedString += string[i];
 
+     }
+    console.log(reversedString);
+    
 };
+reverseString("Hello")
 
 // Do not edit below this line
 module.exports = reverseString;
