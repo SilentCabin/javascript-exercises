0b3c764967542cbfbfe3aa3d3d50bf15611e72b5
SilentCabin <75711387+SilentCabin@users.noreply.github.com>
2024-09-16 04:02:43 +0100

    <feat> complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,14 +1,13 @@
 const reverseString = function(string) {
     let stringLength = string.length;
     let reversedString = "";
-    for (let i = stringLength -1; i >= 0; i--) {
+    for (let i = stringLength -1; i >= 0; i--) { // stringLength - 1 because length is 5 but in an array the 1st letter is 0 not 1
         reversedString += string[i];
 
      }
-    console.log(reversedString);
+    return(reversedString);
     
 };
-reverseString("Hello")
 
 // Do not edit below this line
 module.exports = reverseString;
