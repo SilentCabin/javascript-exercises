6c19042bf79df405402d96d1a1c446459263f3a8
SilentCabin <75711387+SilentCabin@users.noreply.github.com>
2024-09-23 23:15:25 +0100

    <feat> finish excercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    array.forEach((element) => {
+        // if(element.includes(args)) {   // this just did not want to work.
+        //     newArray.push(element);
+        // }
+        if(!args.includes(element)) { // check to see if element is NOT in args
+            newArray.push(element) // only push element if not in args
+        
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
