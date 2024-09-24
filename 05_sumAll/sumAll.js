168d357f18a95190006b4f342a4e20ac88090a1d
SilentCabin <75711387+SilentCabin@users.noreply.github.com>
2024-09-24 02:33:51 +0100

    <feat> create if statement for num1 < num2

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,13 @@
 const sumAll = function(num1, num2) {
     let total = 0;
-    for (let i = num1; i <= num2; i++) { // create a loop for all numbers between num1 and num2
-        total += i;
+    if(num1 < num2){
+        for (let i = num1; i <= num2; i++) { // create a loop for all numbers between num1 and num2
+            total += i;
+        }
+        console.log(total);
+        return total;
     }
-    console.log(total);
-    return total;
 };
-sumAll(1,100);
+sumAll(1,3);
 // Do not edit below this line
 module.exports = sumAll;
