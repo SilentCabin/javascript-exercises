78f808709d8d812ac8a0393e1ce798da9f598d74
SilentCabin <75711387+SilentCabin@users.noreply.github.com>
2024-09-24 03:11:04 +0100

    <feat> finish all exercise 05 and tidy up code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,41 @@
 const sumAll = function(num1, num2) {
+
+    // Check if num1 and num2 are valid inputs
+    if (typeof num1 !== 'number' || 
+        typeof num2 !== 'number' || 
+        num1 < 0 || 
+        num2 < 0 || 
+        !Number.isInteger(num1) || 
+        !Number.isInteger(num2)) {
+        
+        console.log("ERROR");  
+        return "ERROR";        // Return error if invalid input
+    }
+
     let total = 0;
-    if(num1 < num2){
-        for (let i = num1; i <= num2; i++) { // create a loop for all numbers between num1 and num2
+
+    //  When num1 is less than num2
+    if (num1 < num2) {
+        // Sum all numbers between num1 and num2 (inclusive)
+        for (let i = num1; i <= num2; i++) {
             total += i;
         }
-        console.log(total);
-        return total;
-    } else if(num1 > num2) {
+
+        console.log(total);  
+        return total;        
+    } 
+
+    // When num1 is greater than num2
+    else if (num1 > num2) {
+        // Sum all numbers between num2 and num1 (inclusive)
         for (let i = num2; i <= num1; i++) {
-            total += i;            
+            total += i;
         }
-        console.log(total);
-        return total;
-        
+
+        console.log(total);  
+        return total;        
     }
 };
-sumAll(3, 10);
+
 // Do not edit below this line
 module.exports = sumAll;
