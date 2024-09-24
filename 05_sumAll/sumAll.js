c6a9804a26574756c798ad716b11e49385ec3e8d
SilentCabin <75711387+SilentCabin@users.noreply.github.com>
2024-09-24 03:13:30 +0100

    <fix> tidy code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function(num1, num2) {
-
+    
     // Check if num1 and num2 are valid inputs
     if (typeof num1 !== 'number' || 
         typeof num2 !== 'number' || 
@@ -7,35 +7,36 @@ const sumAll = function(num1, num2) {
         num2 < 0 || 
         !Number.isInteger(num1) || 
         !Number.isInteger(num2)) {
+            
+            console.log("ERROR");  
+            return "ERROR";        // Return error if invalid input
+        }
         
-        console.log("ERROR");  
-        return "ERROR";        // Return error if invalid input
-    }
-
-    let total = 0;
-
-    //  When num1 is less than num2
-    if (num1 < num2) {
-        // Sum all numbers between num1 and num2 (inclusive)
-        for (let i = num1; i <= num2; i++) {
-            total += i;
+        let total = 0;
+        
+        //  When num1 is less than num2
+        if (num1 < num2) {
+            // Sum all numbers between num1 and num2 (inclusive)
+            for (let i = num1; i <= num2; i++) {
+                total += i;
+            }
+            
+            console.log(total);  
+            return total;        
+        } 
+        
+        // When num1 is greater than num2
+        else if (num1 > num2) {
+            // Sum all numbers between num2 and num1 (inclusive)
+            for (let i = num2; i <= num1; i++) {
+                total += i;
+            }
+            
+            console.log(total);  
+            return total;        
         }
-
-        console.log(total);  
-        return total;        
-    } 
-
-    // When num1 is greater than num2
-    else if (num1 > num2) {
-        // Sum all numbers between num2 and num1 (inclusive)
-        for (let i = num2; i <= num1; i++) {
-            total += i;
-        }
-
-        console.log(total);  
-        return total;        
-    }
-};
-
-// Do not edit below this line
-module.exports = sumAll;
+    };
+    
+    // Do not edit below this line
+    module.exports = sumAll;
+    
