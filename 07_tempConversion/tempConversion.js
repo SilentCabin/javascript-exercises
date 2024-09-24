291ad153f328de5d76b3078c6fcca3861fe9d4b3
SilentCabin <75711387+SilentCabin@users.noreply.github.com>
2024-09-24 16:42:28 +0100

    <feat> create functions for simple conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,28 @@
-const convertToCelsius = function() {
+// Celcius to Fahrenheit : x °C ≘ (x × ⁠(9/5)⁠ + 32)°F
+
+// Fahrenheit to Celcius :x °F ≘ (x − 32) ×(5/9⁠) °C
+
+const convertToCelsius = function(num) {
+  let conversion = 0;
+
+  conversion = (num - 32) * (5/9);
+
+  console.log(`${num}°F  converted to Celsius is : ${conversion}°C`);
+  return conversion;
+  
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(num) {
+  let conversion = 0;
 
+  conversion = num * (9/5) + 32;
+
+  console.log(`${num}°C converted to Fahrenheit is : ${conversion}°F`);
+  return conversion;
+  };
+
+convertToFahrenheit(100);
+convertToCelsius(0);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
